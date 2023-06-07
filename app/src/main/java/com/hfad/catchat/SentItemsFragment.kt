fc83c4e42b9f49392daee0694ed0821317479517
kaosPower <chenjiax@bu.edu>
2023-06-07 07:22:07 -0400

    sixth push

--- app/src/main/java/com/hfad/catchat/SentItemsFragment.kt
+++ app/src/main/java/com/hfad/catchat/SentItemsFragment.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentContainer
 
 class SentItemsFragment :Fragment(){
-    //testing
     override fun onCreateView(
         inflater: LayoutInflater,container: ViewGroup?,savedInstanceState:Bundle?
     ):View?{
